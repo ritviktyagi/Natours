f85b3457fbe29a05302bdf8ea6666f5dc6e4b11c
Ritvik Tyagi <ritviktyagi@Ritviks-MacBook-Air.local>
2025-10-17 20:46:29 +0530

    improved stripe payment v2

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -12,7 +12,7 @@ const { createBookingCheckout } = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get('/',createBookingCheckout, isLoggedIn, getOverview);
+// router.get('/',createBookingCheckout, isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLogin);
 router.get('/me', protect, getAccount);
